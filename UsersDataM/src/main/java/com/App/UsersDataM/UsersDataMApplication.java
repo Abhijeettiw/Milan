d2c3269a354067fe481dce65fb0796f598628e6e
Abhijeet <abhijeet1992tiwari@gmail.com>
2023-12-31 20:14:43 +0530

    new module

---
+++ UsersDataM/src/main/java/com/App/UsersDataM/UsersDataMApplication.java
@@ -0,0 +1,13 @@
+package com.App.UsersDataM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersDataMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersDataMApplication.class, args);
+	}
+
+}
---
+++ UsersDataM/src/test/java/com/App/UsersDataM/UsersDataMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.UsersDataM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersDataMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
