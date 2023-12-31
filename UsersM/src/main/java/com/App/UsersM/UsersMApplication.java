b93866f886f5c6f2783f46b572dc99e422542e9e
Abhijeet <abhijeet1992tiwari@gmail.com>
2023-12-31 20:10:07 +0530

    Starting

---
+++ UsersM/src/main/java/com/App/UsersM/UsersMApplication.java
@@ -0,0 +1,13 @@
+package com.App.UsersM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMApplication.class, args);
+	}
+
+}
---
+++ UsersM/src/test/java/com/App/UsersM/UsersMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.UsersM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
